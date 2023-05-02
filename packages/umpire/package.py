d8dc444303abf612d1ffb4958d2c1a01d793c07b
Adrien M. BERNEDE <a.bernede@woptim.dev>
2023-05-02 18:16:01 +0200

    Turn device-alloc off by default in Umpire

--- packages/umpire/package.py
+++ packages/umpire/package.py
@@ -93,7 +93,7 @@ class Umpire(CachedCMakePackage, CudaPackage, ROCmPackage):
     variant("tools", default=False, description="Enable tools")
     variant("backtrace", default=False, description="Enable backtrace tools")
     variant("dev_benchmarks", default=False, description="Enable Developer Benchmarks")
-    variant("device_alloc", default=True, description="Enable DeviceAllocator")
+    variant("device_alloc", default=False, description="Enable DeviceAllocator")
     variant("werror", default=True, description="Enable warnings as errors")
     variant("asan", default=False, description="Enable ASAN")
     variant("sanitizer_tests", default=False, description="Enable address sanitizer tests")
