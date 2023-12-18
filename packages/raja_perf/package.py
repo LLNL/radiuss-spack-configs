4542adb7d0cf85b9ec6e5290b7e9e94d4e9d25f1
Adrien M. BERNEDE <51493078+adrienbernede@users.noreply.github.com>
2023-12-18 21:29:16 +0100

    Attempt at defining appropriate MPIEXEC flag for spectrum mpi

--- packages/raja_perf/package.py
+++ packages/raja_perf/package.py
@@ -205,6 +205,9 @@ class RajaPerf(CachedCMakePackage, CudaPackage, ROCmPackage):
         entries = super(RajaPerf, self).initconfig_mpi_entries()
         entries.append(cmake_cache_option("ENABLE_MPI", "+mpi" in spec))
 
+        if spec["mpi"].satisfies("spectrum-mpi");
+            entries.append(cmake_cache_string("MPIEXEC_NUMPROC_FLAG", "-n"))
+
         return entries
 
     def initconfig_package_entries(self):
