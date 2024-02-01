7cec56defcb87067e4030bd92f3919e801f5dabd
Adrien Bernede <51493078+adrienbernede@users.noreply.github.com>
2024-02-01 17:48:29 +0100

    Revert "Empty blt_link_helper to test whether it is necessary"
    This reverts commit 96d69daae1268e1a3f1f063a11197e2f51323312.

--- packages/camp/package.py
+++ packages/camp/package.py
@@ -143,48 +143,48 @@ def cuda_for_radiuss_projects(options, spec):
 
 def blt_link_helpers(options, spec, compiler):
     ### From local package:
-#    if compiler.fc:
-#        fortran_compilers = ["gfortran", "xlf"]
-#        if any(f_comp in compiler.fc for f_comp in fortran_compilers) and ("clang" in compiler.cxx):
-#            # Pass fortran compiler lib as rpath to find missing libstdc++
-#            libdir = os.path.join(os.path.dirname(
-#                           os.path.dirname(compiler.fc)), "lib")
-#            flags = ""
-#            for _libpath in [libdir, libdir + "64"]:
-#                if os.path.exists(_libpath):
-#                    flags += " -Wl,-rpath,{0}".format(_libpath)
-#            description = ("Adds a missing libstdc++ rpath")
-#            if flags:
-#                options.append(cmake_cache_string("BLT_EXE_LINKER_FLAGS", flags, description))
-#
-#            # Ignore conflicting default gcc toolchain
-#            options.append(cmake_cache_string("BLT_CMAKE_IMPLICIT_LINK_DIRECTORIES_EXCLUDE",
-#            "/usr/tce/packages/gcc/gcc-4.9.3/lib64;/usr/tce/packages/gcc/gcc-4.9.3/gnu/lib64/gcc/powerpc64le-unknown-linux-gnu/4.9.3;/usr/tce/packages/gcc/gcc-4.9.3/gnu/lib64;/usr/tce/packages/gcc/gcc-4.9.3/lib64/gcc/x86_64-unknown-linux-gnu/4.9.3"))
-#
+    if compiler.fc:
+        fortran_compilers = ["gfortran", "xlf"]
+        if any(f_comp in compiler.fc for f_comp in fortran_compilers) and ("clang" in compiler.cxx):
+            # Pass fortran compiler lib as rpath to find missing libstdc++
+            libdir = os.path.join(os.path.dirname(
+                           os.path.dirname(compiler.fc)), "lib")
+            flags = ""
+            for _libpath in [libdir, libdir + "64"]:
+                if os.path.exists(_libpath):
+                    flags += " -Wl,-rpath,{0}".format(_libpath)
+            description = ("Adds a missing libstdc++ rpath")
+            if flags:
+                options.append(cmake_cache_string("BLT_EXE_LINKER_FLAGS", flags, description))
+
+            # Ignore conflicting default gcc toolchain
+            options.append(cmake_cache_string("BLT_CMAKE_IMPLICIT_LINK_DIRECTORIES_EXCLUDE",
+            "/usr/tce/packages/gcc/gcc-4.9.3/lib64;/usr/tce/packages/gcc/gcc-4.9.3/gnu/lib64/gcc/powerpc64le-unknown-linux-gnu/4.9.3;/usr/tce/packages/gcc/gcc-4.9.3/gnu/lib64;/usr/tce/packages/gcc/gcc-4.9.3/lib64/gcc/x86_64-unknown-linux-gnu/4.9.3"))
+
     compilers_using_toolchain = ["pgc++", "xlc++", "xlC_r", "icpc", "clang++", "icpx"]
-#    if any(tc_comp in compiler.cxx for tc_comp in compilers_using_toolchain):
-#        if spec_uses_toolchain(spec) or spec_uses_gccname(spec):
-#
-#            # Ignore conflicting default gcc toolchain
-#            options.append(cmake_cache_string("BLT_CMAKE_IMPLICIT_LINK_DIRECTORIES_EXCLUDE",
-#            "/usr/tce/packages/gcc/gcc-4.9.3/lib64;/usr/tce/packages/gcc/gcc-4.9.3/gnu/lib64/gcc/powerpc64le-unknown-linux-gnu/4.9.3;/usr/tce/packages/gcc/gcc-4.9.3/gnu/lib64;/usr/tce/packages/gcc/gcc-4.9.3/lib64/gcc/x86_64-unknown-linux-gnu/4.9.3"))
-#
-#    if "cce" in compiler.cxx:
-#        description = (
-#            "Adds a missing rpath for libraries " "associated with the fortran compiler"
-#        )
-#        # Here is where to find libs that work for fortran
-#        libdir = "/opt/cray/pe/cce/{0}/cce-clang/x86_64/lib".format(compiler.version)
-#        linker_flags = "${{BLT_EXE_LINKER_FLAGS}} -Wl,-rpath,{0}".format(libdir)
-#
-#        version = "{0}".format(compiler.version)
-#
-#        if version == "16.0.0" or version == "16.0.1":
-#            # Here is another directory added by cce@16.0.0 and cce@16.0.1
-#            libdir = os.path.join(libdir,"x86_64-unknown-linux-gnu")
-#            linker_flags += " -Wl,-rpath,{0}".format(libdir)
-#
-#        options.append(cmake_cache_string("BLT_EXE_LINKER_FLAGS", linker_flags, description))
+    if any(tc_comp in compiler.cxx for tc_comp in compilers_using_toolchain):
+        if spec_uses_toolchain(spec) or spec_uses_gccname(spec):
+
+            # Ignore conflicting default gcc toolchain
+            options.append(cmake_cache_string("BLT_CMAKE_IMPLICIT_LINK_DIRECTORIES_EXCLUDE",
+            "/usr/tce/packages/gcc/gcc-4.9.3/lib64;/usr/tce/packages/gcc/gcc-4.9.3/gnu/lib64/gcc/powerpc64le-unknown-linux-gnu/4.9.3;/usr/tce/packages/gcc/gcc-4.9.3/gnu/lib64;/usr/tce/packages/gcc/gcc-4.9.3/lib64/gcc/x86_64-unknown-linux-gnu/4.9.3"))
+
+    if "cce" in compiler.cxx:
+        description = (
+            "Adds a missing rpath for libraries " "associated with the fortran compiler"
+        )
+        # Here is where to find libs that work for fortran
+        libdir = "/opt/cray/pe/cce/{0}/cce-clang/x86_64/lib".format(compiler.version)
+        linker_flags = "${{BLT_EXE_LINKER_FLAGS}} -Wl,-rpath,{0}".format(libdir)
+
+        version = "{0}".format(compiler.version)
+
+        if version == "16.0.0" or version == "16.0.1":
+            # Here is another directory added by cce@16.0.0 and cce@16.0.1
+            libdir = os.path.join(libdir,"x86_64-unknown-linux-gnu")
+            linker_flags += " -Wl,-rpath,{0}".format(libdir)
+
+        options.append(cmake_cache_string("BLT_EXE_LINKER_FLAGS", linker_flags, description))
 
 
 class Camp(CMakePackage, CudaPackage, ROCmPackage):
