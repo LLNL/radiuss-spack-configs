841f99671ca0d8bf040f48b07649f06aa0431f51
Adrien M. BERNEDE <51493078+adrienbernede@users.noreply.github.com>
2024-03-28 11:19:12 +0100

    MPI handling, spread the word: umpire and caliper

--- packages/caliper/package.py
+++ packages/caliper/package.py
@@ -175,6 +175,18 @@ class Caliper(CachedCMakePackage, CudaPackage, ROCmPackage):
         entries = super(Caliper, self).initconfig_mpi_entries()
 
         entries.append(cmake_cache_option("WITH_MPI", "+mpi" in spec))
+        if "+mpi" in spec:
+            # Replace /usr/bin/srun path with srun flux wrapper path on TOSS 4
+            # TODO: Remove this logic by adding `using_flux` case in
+            #  spack/lib/spack/spack/build_systems/cached_cmake.py:196 and remove hard-coded
+            #  path to srun in same file.
+            if "toss_4" in self._get_sys_type(spec):
+                srun_wrapper = which_string("srun")
+                mpi_exec_index = [
+                    index for index, entry in enumerate(entries) if "MPIEXEC_EXECUTABLE" in entry
+                ]
+                del entries[mpi_exec_index[0]]
+                entries.append(cmake_cache_path("MPIEXEC_EXECUTABLE", srun_wrapper))
 
         return entries
 
