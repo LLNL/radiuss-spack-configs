1527fe4286c13e93f0edb722a61510ac8e6765a3
David Beckingsale <davidbeckingsale@gmail.com>
2024-07-11 08:31:08 -0700

    Use library version of fmt with CHAI+CUDA build

--- packages/chai/package.py
+++ packages/chai/package.py
@@ -150,6 +150,8 @@ class Chai(CachedCMakePackage, CudaPackage, ROCmPackage):
         depends_on("umpire+cuda")
         for sm_ in CudaPackage.cuda_arch_values:
             depends_on("umpire+cuda cuda_arch={0}".format(sm_), when="cuda_arch={0}".format(sm_))
+        with when("@2024.02.0:")
+            depends_on("umpire~fmt_header_only")
 
     with when("+rocm"):
         depends_on("umpire+rocm")
