4221ef69db81fb1bc666c84bfda67e38bd87411b
Rich Hornung <hornung1@llnl.gov>
2024-08-05 13:02:11 -0700

    Update version dependencies on RAJA and Umpire

--- packages/chai/package.py
+++ packages/chai/package.py
@@ -142,6 +142,7 @@ class Chai(CachedCMakePackage, CudaPackage, ROCmPackage):
     conflicts("^blt@:0.3.6", when="+rocm")
 
     depends_on("umpire")
+    depends_on("umpire@2024.07.0:", when="@2024.07.0:")
     depends_on("umpire@2024.02.1:", when="@2024.02.1:")
     depends_on("umpire@2024.02.0:", when="@2024.02.0:")
     depends_on("umpire@2023.06.0", when="@2023.06.0")
@@ -169,6 +170,7 @@ class Chai(CachedCMakePackage, CudaPackage, ROCmPackage):
     with when("+raja"):
         depends_on("raja~openmp", when="~openmp")
         depends_on("raja+openmp", when="+openmp")
+        depends_on("raja@2024.07.2:", when="@2024.02.7:")
         depends_on("raja@2024.02.2:", when="@2024.02.2:")
         depends_on("raja@2024.02.1:", when="@2024.02.1:")
         depends_on("raja@2024.02.0:", when="@2024.02.0:")
