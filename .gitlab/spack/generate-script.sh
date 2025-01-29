6c0533c9a59237ad5d265d79f6ad535e987b03c8
Adrien M. BERNEDE <51493078+adrienbernede@users.noreply.github.com>
2025-01-29 17:01:33 +0100

    Revert previous commit: Spack CI _needs_ a mirror...

--- .gitlab/spack/generate-script.sh
+++ .gitlab/spack/generate-script.sh
@@ -11,9 +11,7 @@ hostname
 . ${MY_SPACK_PARENT_DIR}/spack/share/spack/setup-env.sh
 spack ${MY_SPACK_DEBUG} env activate --without-view .gitlab/spack/envs/${MY_ENV_NAME}
 spack ${MY_SPACK_DEBUG} config blame mirrors
-# No binary cache here: I prefer to be safe and reinstall everything.
-# E.g., a change in CachedCMakePackage would not trigger the rebuild of our packages, which is a problem.
-#spack ${MY_SPACK_DEBUG} mirror add --oci-username ${CI_REGISTRY_USER} --oci-password ${CI_REGISTRY_PASSWORD} buildcache-destination oci://${CI_REGISTRY_IMAGE}/${SPACK_TARGET}
+spack ${MY_SPACK_DEBUG} mirror add --oci-username ${CI_REGISTRY_USER} --oci-password ${CI_REGISTRY_PASSWORD} buildcache-destination oci://${CI_REGISTRY_IMAGE}/${SPACK_TARGET}
 spack ${MY_SPACK_DEBUG} config blame mirrors
 spack ${MY_SPACK_DEBUG} concretize || spack ${MY_SPACK_DEBUG} concretize
 spack ${MY_SPACK_DEBUG} --color=always --config-scope "${CI_PROJECT_DIR}/.gitlab/spack" ci generate --check-index-only --artifacts-root "${CI_PROJECT_DIR}/jobs_scratch_dir" --output-file "${CI_PROJECT_DIR}/jobs_scratch_dir/pipeline.yml"
