c5ed432aa5d9645ffa60a3a4f62031464af2d8c6
Adrien M. BERNEDE <51493078+adrienbernede@users.noreply.github.com>
2025-03-19 16:25:40 +0100

    Add package repository to concrete env directory

--- .gitlab/spack/generate-script.sh
+++ .gitlab/spack/generate-script.sh
@@ -16,3 +16,5 @@ spack ${MY_SPACK_DEBUG} config blame mirrors
 spack ${MY_SPACK_DEBUG} concretize || spack ${MY_SPACK_DEBUG} concretize
 spack ${MY_SPACK_DEBUG} --color=always --config-scope "${CI_PROJECT_DIR}/.gitlab/spack" ci generate --check-index-only --artifacts-root "${CI_PROJECT_DIR}/jobs_scratch_dir" --output-file "${CI_PROJECT_DIR}/jobs_scratch_dir/pipeline.yml"
 cp -rL .gitlab/spack/envs/shared-ci/${LCSCHEDCLUSTER} ${CI_PROJECT_DIR}/jobs_scratch_dir/concrete_environment/${LCSCHEDCLUSTER}
+cp -rL .gitlab/spack/envs/shared-ci/repo.yaml ${CI_PROJECT_DIR}/jobs_scratch_dir/concrete_environment/repo.yaml
+cp -rL .gitlab/spack/envs/shared-ci/packages ${CI_PROJECT_DIR}/jobs_scratch_dir/concrete_environment/packages
