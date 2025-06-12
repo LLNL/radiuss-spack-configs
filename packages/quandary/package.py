62d8cb0ca22a6b6379124082094dbe1b20ff7d69
Tara Drwenski <drwenski1@llnl.gov>
2025-06-12 14:35:40 -0600

    Use build_type=Debug instead of new debug variant and add int64 variant

--- packages/quandary/package.py
+++ packages/quandary/package.py
@@ -20,14 +20,15 @@ class Quandary(CachedCMakePackage, CudaPackage, ROCmPackage):
     version("learning", branch="learning")
 
     variant("slepc", default=False, description="Build with SLEPc library")
-    variant("debug", default=False, description="Debug mode")
+    variant("int64", default=False, description="Use 64 bit ints for PetscInts")
     variant("test", default=False, description="Add dependencies needed for testing")
 
     depends_on("cxx", type="build")
     depends_on("c", type="build")
 
     depends_on("petsc~hypre~metis~fortran")
-    depends_on("petsc+debug", when="+debug")
+    depends_on("petsc+debug", when="build_type=Debug")
+    depends_on("petsc+int64", when="+int64")
     depends_on("slepc", when="+slepc")
     depends_on("mpi", type=("build", "link", "run"))
 
