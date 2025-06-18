2eea35fccfc1062f3f6b2a695fca5608824f53ec
john bowen <john.bowen42@gmail.com>
2025-06-18 16:58:08 -0700

    Fix versioning

--- packages/raja-perf/package.py
+++ packages/raja-perf/package.py
@@ -336,11 +336,12 @@ class RajaPerf(CachedCMakePackage, CudaPackage, ROCmPackage):
         entries.append(cmake_cache_option("RAJA_ENABLE_SYCL", spec.satisfies("+sycl")))
 
         # C++17
-        if spec.satisfies("@2024.07.0:") and spec.satisfies("+sycl"):
+        if (spec.satisfies("@develop") or
+            (spec.satisfies("@2024.07.0:") and spec.satisfies("+sycl"))):
             entries.append(cmake_cache_string("BLT_CXX_STD", "c++17"))
-        # C++14
+        # C++17
         # Using RAJA version as threshold on purpose (no 0.14 version of RAJAPerf were released).
-        elif spec.satisfies("@0.14.0:"):
+        elif spec.satisfies("@0.14.0:2025.03.0"):
             entries.append(cmake_cache_string("BLT_CXX_STD", "c++14"))
 
         entries.append(cmake_cache_option("ENABLE_BENCHMARKS", "tests=benchmarks" in spec))
