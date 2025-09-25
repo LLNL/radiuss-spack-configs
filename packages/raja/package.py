b78a9a9504fd77abc904d5c5a5ad182db4165daa
Brian Han <han12@llnl.gov>
2025-09-25 09:59:12 -0700

    Missing camp colon

--- packages/raja/package.py
+++ packages/raja/package.py
@@ -252,7 +252,7 @@ class Raja(CachedCMakePackage, CudaPackage, ROCmPackage):
     depends_on("camp@2025.09.2:", when="@2025.09.0:")
     depends_on("camp@2025.03.0:", when="@2025.03.0:")
     depends_on("camp@2024.07.0:", when="@2024.07.0:")
-    depends_on("camp@2024.02.1", when="@2024.02.1:")
+    depends_on("camp@2024.02.1:", when="@2024.02.1:")
     depends_on("camp@2024.02.0", when="@2024.02.0")
     depends_on("camp@2023.06.0", when="@2023.06.0:2023.06.1")
     depends_on("camp@2022.10.1:2023.06.0", when="@2022.10.3:2022.10.5")
